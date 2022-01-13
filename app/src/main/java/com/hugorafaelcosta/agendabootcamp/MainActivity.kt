a6583a6701fbc044b79e3ba1bd2b86c4bd2d2e6b
Hugo Costa <costarafaelhugo@gmail.com>
2022-01-13 11:36:38 -0300

    Adicionei a intent e os recursos nativos do android

--- app/src/main/java/com/hugorafaelcosta/agendabootcamp/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/agendabootcamp/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.hugorafaelcosta.agendabootcamp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.CONTENT_URI
+import android.provider.ContactsContract.CommonDataKinds.Organization.TITLE
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.provider.CalendarContract.Events.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +18,14 @@ class MainActivity : AppCompatActivity() {
         val btnSetEvent = findViewById(R.id.set_event) as Button
         btnSetEvent.setOnClickListener {
 
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Bpptcamp everis")
+                .putExtra(EVENT_LOCATION, "on line")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,System.currentTimeMillis() + (60*60*1000))
+
+            startActivity(intent)
         }
     }
 }
